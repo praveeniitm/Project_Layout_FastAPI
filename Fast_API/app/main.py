c89ef11b9c766218c0e6666a0e2644230a45a1b7
praveeniitm <51504158+praveeniitm@users.noreply.github.com>
2022-07-03 15:10:26 +0530

    Update main.py

--- Fast_API/app/main.py
+++ Fast_API/app/main.py
@@ -6,6 +6,13 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
+from app.api.endpoints.api import api_router
+import uvicorn
 
-app = FastAPI()
 
+app = FastAPI(title = "Project name")
+
+app.include_router(api_router)
+
+if __name__ = '__main__':
+  uvicorn.run(app,host = 0.0.0.0, port=8000)
