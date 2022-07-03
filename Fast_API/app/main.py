90763e962548388a836bfe626277ce681bb79b91
praveeniitm <51504158+praveeniitm@users.noreply.github.com>
2022-07-03 14:30:17 +0530

    Update main.py

--- Fast_API/app/main.py
+++ Fast_API/app/main.py
@@ -1,1 +1,11 @@
+# This is the file, from where service/application will start.
+
+
+
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+app = FastAPI()
 
